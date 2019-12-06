4535ff07d6347b3e999dffb6d5b1d6a98f342320
wooleah <woojaeizzle@gmail.com>
2019-12-06 09:55:18 +0900

    Update day5.js
    got a wrong answer, need to fix it

--- day5.js
+++ day5.js
@@ -1,82 +1,88 @@
-// 1. n + 4(n=0,1,2,...) is the protocol element
-// 2. other elements are positions in input array
-const input = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,19,10,23,2,13,23,27,1,5,27,31,2,6,31,35,1,6,35,39,2,39,9,43,1,5,43,47,1,13,47,51,1,10,51,55,2,55,10,59,2,10,59,63,1,9,63,67,2,67,13,71,1,71,6,75,2,6,75,79,1,5,79,83,2,83,9,87,1,6,87,91,2,91,6,95,1,95,6,99,2,99,13,103,1,6,103,107,1,2,107,111,1,111,9,0,99,2,14,0,0];
+const input = [3,225,1,225,6,6,1100,1,238,225,104,0,1102,67,92,225,1101,14,84,225,1002,217,69,224,101,-5175,224,224,4,224,102,8,223,223,101,2,224,224,1,224,223,223,1,214,95,224,101,-127,224,224,4,224,102,8,223,223,101,3,224,224,1,223,224,223,1101,8,41,225,2,17,91,224,1001,224,-518,224,4,224,1002,223,8,223,101,2,224,224,1,223,224,223,1101,37,27,225,1101,61,11,225,101,44,66,224,101,-85,224,224,4,224,1002,223,8,223,101,6,224,224,1,224,223,223,1102,7,32,224,101,-224,224,224,4,224,102,8,223,223,1001,224,6,224,1,224,223,223,1001,14,82,224,101,-174,224,224,4,224,102,8,223,223,101,7,224,224,1,223,224,223,102,65,210,224,101,-5525,224,224,4,224,102,8,223,223,101,3,224,224,1,224,223,223,1101,81,9,224,101,-90,224,224,4,224,102,8,223,223,1001,224,3,224,1,224,223,223,1101,71,85,225,1102,61,66,225,1102,75,53,225,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,8,226,226,224,102,2,223,223,1005,224,329,1001,223,1,223,1108,677,677,224,1002,223,2,223,1006,224,344,101,1,223,223,1007,226,677,224,102,2,223,223,1005,224,359,101,1,223,223,1007,677,677,224,1002,223,2,223,1006,224,374,101,1,223,223,1108,677,226,224,1002,223,2,223,1005,224,389,1001,223,1,223,108,226,677,224,102,2,223,223,1006,224,404,101,1,223,223,1108,226,677,224,102,2,223,223,1005,224,419,101,1,223,223,1008,677,677,224,102,2,223,223,1005,224,434,101,1,223,223,7,677,226,224,1002,223,2,223,1005,224,449,101,1,223,223,1008,226,226,224,102,2,223,223,1005,224,464,1001,223,1,223,107,226,677,224,1002,223,2,223,1006,224,479,1001,223,1,223,107,677,677,224,102,2,223,223,1005,224,494,1001,223,1,223,1008,226,677,224,102,2,223,223,1006,224,509,1001,223,1,223,1107,677,226,224,102,2,223,223,1005,224,524,101,1,223,223,1007,226,226,224,1002,223,2,223,1006,224,539,1001,223,1,223,107,226,226,224,102,2,223,223,1006,224,554,101,1,223,223,108,677,677,224,1002,223,2,223,1006,224,569,1001,223,1,223,7,226,677,224,102,2,223,223,1006,224,584,1001,223,1,223,8,677,226,224,102,2,223,223,1005,224,599,101,1,223,223,1107,677,677,224,1002,223,2,223,1005,224,614,101,1,223,223,8,226,677,224,102,2,223,223,1005,224,629,1001,223,1,223,7,226,226,224,1002,223,2,223,1006,224,644,1001,223,1,223,108,226,226,224,1002,223,2,223,1006,224,659,101,1,223,223,1107,226,677,224,1002,223,2,223,1006,224,674,101,1,223,223,4,223,99,226];
 
 const opcode = {
   ADD: 1,
   MULTIPLY: 2,
+  INPUT: 3,
+  OUTPUT: 4,
   HALT: 99
 }
+const paramMode = {
+  POSITION: 0,
+  IMMEDIATE: 1
+}
 
-// Part 1
-// before running the program, replace position 1 with the value 12 and replace position 2 with the value 2. What value is left at position 0 after the program halts
+const AIR_CONDITIONER_UNIT_ID = 1;
+
 function getOutput(input) {
   let inputArr = [...input];
 
   // opcode
   let currentOpcodePos = 0;
+  let nextOpcodePos;
+  let opcodeInput;
+  let opcodeProtocol;
   // parameters
+  let input1;
   let input1Pos = 0;
+  let input1Mode;
+  let input2;
   let input2Pos = 0;
+  let input2Mode;
   let outputPos = 0;
+  let output;
   
-  while(currentOpcodePos < inputArr.length && inputArr[currentOpcodePos] !== opcode.HALT) {
-    input1Pos = inputArr[currentOpcodePos + 1];
-    input2Pos = inputArr[currentOpcodePos + 2];
-    outputPos = inputArr[currentOpcodePos + 3];
-    switch (inputArr[currentOpcodePos]) {
+  while(currentOpcodePos < inputArr.length) {
+    // analyze opcode
+    opcodeInput = inputArr[currentOpcodePos].toString();
+    opcodeProtocol = Number(opcodeInput.slice(-2));
+    input1Mode = opcodeInput[opcodeInput.length - 3] ? paramMode.IMMEDIATE : paramMode.POSITION;
+    input2Mode = opcodeInput[opcodeInput.length - 4] ? paramMode.IMMEDIATE : paramMode.POSITION;
+    
+    switch (opcodeProtocol) {
+      case opcode.INPUT:
+        outputPos = inputArr[currentOpcodePos + 1];
+        inputArr[outputPos] = AIR_CONDITIONER_UNIT_ID;
+        nextOpcodePos = 2;
+        break;
+      case opcode.OUTPUT:
+        debugger;
+        input1Pos = inputArr[currentOpcodePos + 1];
+        output = inputArr[input1Pos];
+        nextOpcodePos = 2;
+        break;
       case opcode.ADD:
-        inputArr[outputPos] = inputArr[input1Pos] + inputArr[input2Pos];
-        break;
       case opcode.MULTIPLY:
-        inputArr[outputPos] = inputArr[input1Pos] * inputArr[input2Pos];
+        if (input1Mode === paramMode.POSITION) {
+          input1Pos = inputArr[currentOpcodePos + 1];
+          input1 = inputArr[input1Pos];
+        } else if (input1Mode === paramMode.IMMEDIATE) {
+          input1 = inputArr[currentOpcodePos + 1];
+        }
+
+        if (input2Mode === paramMode.POSITION) {
+          input2Pos = inputArr[currentOpcodePos + 2];
+          input2 = inputArr[input2Pos];
+        } else if (input1Mode === paramMode.IMMEDIATE) {
+          input2 = inputArr[currentOpcodePos + 2];
+        }
+
+        outputPos = inputArr[currentOpcodePos + 3];
+
+        if (opcodeProtocol === opcode.ADD) {
+          inputArr[outputPos] = input1 + input2;
+        } else if (opcodeProtocol === opcode.MULTIPLY) {
+          inputArr[outputPos] = input1 * input2;
+        }
+        nextOpcodePos = 4;
         break;
+      case opcode.HALT:
+        return output;
     }
-    currentOpcodePos += 4;
+    currentOpcodePos += nextOpcodePos;
   }
-  return inputArr[0];
+
+  return output;
 }
 
-const tweakInputArrV1 = (inputArr) => {
-  // noun (0 ~ 99)
-  inputArr[1] = 12;
-  // verb (0 ~ 99)
-  inputArr[2] = 2;
-}
-// tweakInputArrV1(input);
-// getOutput(input);
-
-// Part 2
-// determine what pair of inputs produces the output 19690720
-// What is 100 * noun + verb?
-function solution(input) {
-  const MIN_NOUN = 0;
-  const MAX_NOUN = 99;
-  const MIN_VERB = 0;
-  const MAX_VERB = 99;
-  const answer = 19690720;
-  const inputArr = [...input];
-
-  let originalNoun = inputArr[1];
-  let originalVerb = inputArr[2];
-  function tweakInputArrV2(noun, verb) {
-    // noun (0 ~ 99)
-    inputArr[1] = noun;
-    // verb (0 ~ 99)
-    inputArr[2] = verb;
-  }
-  
-  let output = 0;
-  for (let noun = MIN_NOUN; noun <= MAX_NOUN; ++noun) {
-    for (let verb = MIN_VERB; verb <= MAX_VERB; ++verb) {
-      tweakInputArrV2(originalNoun, originalVerb);
-      tweakInputArrV2(noun, verb);
-      output = getOutput(inputArr);
-      if (output === answer) {
-        return 100 * noun + verb;
-      }
-    }
-  }
-}
-
-solution(input);
+getOutput(input);
